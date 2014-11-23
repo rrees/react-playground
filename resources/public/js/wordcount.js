d643d5f7322330b073c7a043aa36af36651ec078
Robert Rees <shudderfix@gmail.com>
2014-11-23 19:00:08 +0000

    Creates a working basic wordcount

--- resources/public/js/wordcount.js
+++ resources/public/js/wordcount.js
@@ -1,18 +1,28 @@
 
-console.log('Hello word counter');
-
 var Editor = React.createClass({
 	render: function() {
+		var text = this.state.text;
+		var wordcount = text ? text.match(/\w+/g).length : 0;
+
 		return React.DOM.div({},
-			React.DOM.textarea(),
-			React.createElement(Count)
+			React.DOM.textarea({onChange: this.handleChange}),
+			React.createElement(Count, {count: wordcount})
 			);
+	},
+	getInitialState: function() {
+		return {text: ''};
+	},
+	handleChange: function (event) {
+		this.setState({text: event.target.value});
 	}
 });
 
 var Count = React.createClass({
 	render: function() {
-		return React.DOM.div({}, 0);
+		return React.DOM.div({}, this.props.count);
+	},
+	getDefaultProps: function() {
+		return {count: 0};
 	}
 });
 
